4fcd0a389f487bb66b044d26eac52bca5f5ef601
bach9w <78446671+bach9w@users.noreply.github.com>
2023-12-19 17:21:21 +0200

    Refactor present-hero component

--- src/components/Front-page/present-hero.tsx
+++ src/components/Front-page/present-hero.tsx
@@ -5,7 +5,7 @@ const PresentHero = () => {
 	return (
 		<>
 			<div>
-				<div className="mt-0 grid grid-col-3 align-center min-w-[550px] h-[20px] ">
+				<div className="mt-0 grid grid-col-3 align-center h-[20px] ">
 					<div className=" text text-xl font-normal bg-white text-black uppercase">
 						The most optimal way of travel
 					</div>
